f06eaaa07f0be9cf1e3fc25f4621c6fe7cb0cad8
vivihelo <134708030+vivihelo@users.noreply.github.com>
2024-09-25 10:48:44 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,11 +4,19 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url)
-
     const dados = await res.json()
+    const pessoaConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
 
-    console.log(dados);
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_conectadas}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
 
+    // código omitido
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+
+    
 }
 
 visualizarInformacoesGlobais()

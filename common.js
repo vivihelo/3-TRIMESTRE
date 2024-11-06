8be418bcf23c8b00b3d7d64e143d13446d421ec3
vivihelo <134708030+vivihelo@users.noreply.github.com>
2024-11-06 10:40:47 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,12 @@
 const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
-  };
-  
-  export {getCSS}
+  const bodyStyles = getComputedStyle(document.body)
+  return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+  family: getCSS('--font'),
+  size: 16,
+  color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
